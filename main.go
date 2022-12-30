f6ec0ff61a7b18bc95005f86cd6126cf0eaadadc
Thomas <thomastayac@gmail.com>
2022-12-30 01:47:59 -0800

    basic repl

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -13,9 +15,34 @@ import (
 )
 
 func main() {
-	expr, err := parser.ParseString(strings.Join(os.Args[1:], " "))
+	shouldSeed := flag.Bool("seed", true, "Seed the VM")
+	flag.Parse()
+	m := vm.New(vm.Params{})
+	var env vm.VMEnv
+	if *shouldSeed {
+		env = seedEnv
+	}
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(">>> ")
+		text, _ := reader.ReadString('\n')
+		if text == "\n" {
+			continue
+		}
+		if strings.HasPrefix(text, "var") {
+			// var decl
+			panic("vardecl")
+		} else {
+			runOnString(text, m, env)
+		}
+	}
+}
+
+func runOnString(s string, m vm.VMState, env vm.VMEnv) {
+	expr, err := parser.ParseString(s)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Parse Error:", err)
+		return
 	}
 	fmt.Println("Expression:", expr.String())
 	comp := compiler.Compile(expr)
@@ -23,50 +50,51 @@ func main() {
 		for _, compErr := range comp.Errors {
 			log.Println(compErr)
 		}
-		os.Exit(1)
+		return
 	}
 	fmt.Println("Bytecode:")
 	for i, b := range comp.Bytecode {
 		fmt.Println("  ", i, b.String())
 	}
-	m := vm.New(vm.Params{})
-	// seed the VM with some useful variables
-	fooObjVal := map[string]bytecode.BVal{
-		"bar": bytecode.BInt(10),
-		"baz": vm.WrapFn("baz", func(x bytecode.BVal) bytecode.BVal {
-			log.Println(x)
-			xx := x.(bytecode.BInt)
-			return bytecode.BFloat(float64(xx) * 43.4)
-		}),
-	}
-	env := vm.VMEnv{
-		"a":        bytecode.BInt(43),
-		"b":        bytecode.BInt(2),
-		"c":        bytecode.BInt(15),
-		"foo":      bytecode.BFloat(10.5),
-		"s":        bytecode.BStr("I am a string!"),
-		"fizz":     bytecode.BStr("fizz"),
-		"buzz":     bytecode.BStr("buzz"),
-		"fizzbuzz": bytecode.BStr("fizzbuzz"),
-		"emptyObj": nil,
-		"fooObj":   bytecode.BObj(fooObjVal),
-		// functions
-		"foobar": vm.WrapFn("foobar", func(x bytecode.BVal) bytecode.BVal {
-			log.Println(x)
-			return bytecode.BNull{}
-		}),
-		"ba": vm.WrapFn("ba", func(x bytecode.BVal) (bytecode.BVal, error) {
-			log.Println(x)
-			xx := x.(bytecode.BInt)
-			return bytecode.BFloat(float64(xx) * 43.4), nil
-		}),
-		"vv": vm.WrapFn("vv", func(x bytecode.BVal) {
-			log.Println(x)
-		}),
-	}
 	result, err := m.Eval(comp, env)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	fmt.Println("VM result:", result.Val)
+}
+
+// seed the VM with some useful variables
+var fooObjVal = map[string]bytecode.BVal{
+	"bar": bytecode.BInt(10),
+	"baz": vm.WrapFn("baz", func(x bytecode.BVal) bytecode.BVal {
+		log.Println(x)
+		xx := x.(bytecode.BInt)
+		return bytecode.BFloat(float64(xx) * 43.4)
+	}),
+}
+var seedEnv = vm.VMEnv{
+	"a":        bytecode.BInt(43),
+	"b":        bytecode.BInt(2),
+	"c":        bytecode.BInt(15),
+	"foo":      bytecode.BFloat(10.5),
+	"s":        bytecode.BStr("I am a string!"),
+	"fizz":     bytecode.BStr("fizz"),
+	"buzz":     bytecode.BStr("buzz"),
+	"fizzbuzz": bytecode.BStr("fizzbuzz"),
+	"emptyObj": nil,
+	"fooObj":   bytecode.BObj(fooObjVal),
+	// functions
+	"foobar": vm.WrapFn("foobar", func(x bytecode.BVal) bytecode.BVal {
+		log.Println(x)
+		return bytecode.BNull{}
+	}),
+	"ba": vm.WrapFn("ba", func(x bytecode.BVal) (bytecode.BVal, error) {
+		log.Println(x)
+		xx := x.(bytecode.BInt)
+		return bytecode.BFloat(float64(xx) * 43.4), nil
+	}),
+	"vv": vm.WrapFn("vv", func(x bytecode.BVal) {
+		log.Println(x)
+	}),
 }
